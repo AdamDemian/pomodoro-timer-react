3205ef74c96616a11e54b0543a8f69668d384f8f
AdamDemian <adamdemian1@gmail.com>
2021-08-10 18:39:37 +0200

    initial commit

---
+++ client/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Pomodoro from './componets/Pomodoro';
+
+function App() {
+  return (
+    <div className="App">
+      <Pomodoro/>
+    </div>
+  );
+}
+
+export default App;
